dc3fefa0909e8a1c9adffdeded69c3e6e32088ce
ss2261 <ss2261@kent.ac.uk>
2021-04-12 11:26:01 +0100

    mainGame - basic functionality done, only rock is working as instead. Need to fix scores.

--- src/mainGame.java
+++ src/mainGame.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class mainGame {
     public static void main(String[] args) {
-        rockPaperScissorsGame();
+        while (true) {
+            rockPaperScissorsGame();
+        }
     }
 
 
@@ -19,6 +21,8 @@ public class mainGame {
         int playerScore = 0;
         int computerScore = 0;
 
+
+        System.out.println("Your score = "+playerScore+"   Computer score = "+computerScore);
         System.out.println();
         System.out.println(computerValue);
         System.out.println("Select below:");
@@ -29,8 +33,20 @@ public class mainGame {
         String userInput = scanner.nextLine();
 
 
-        if (userInput.toLowerCase().equals("r") && computerValue == 1) {
-
+        if (userInput.toLowerCase().equals("r") && computerValue == 1) { // rock vs rock
+            System.out.println("draw");
+        }else if (userInput.toLowerCase().equals("r") && computerValue == 2){ // rock vs paper
+            System.out.println("You lose!");
+            computerScore++;
+            if(playerScore!=0){
+                playerScore--;
+            }
+        }else if (userInput.toLowerCase().equals("r") && computerValue == 3){ // rock vs scissors
+            System.out.println("You win!");
+            playerScore++;
+            if(computerScore!=0){
+                computerScore--;
+            }
         }
 
 
