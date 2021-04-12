9e5d105036f781d009777673b0c9505eb358a523
ss2261 <ss2261@kent.ac.uk>
2021-04-12 11:14:19 +0100

    mainGame - Started work on the main method, i have randomly generated computer choice and I now need to compare the user's choice against this.

--- src/mainGame.java
+++ src/mainGame.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class mainGame {
     public static void main(String[] args) {
-        System.out.println("test");
+        rockPaperScissorsGame();
+    }
+
+
+    public static void menu() {
+
+        System.out.println("Main Menu");
+        System.out.println();
+    }
+
+    public static void rockPaperScissorsGame() {
+
+        int computerValue = (int) Math.floor(Math.random() * (3 - 1 + 1) + 1); // the computer choice
+
+        int playerScore = 0;
+        int computerScore = 0;
+
+        System.out.println();
+        System.out.println(computerValue);
+        System.out.println("Select below:");
+
+        System.out.println();
+        System.out.println("[R] - Rock [P]- Paper [S]- Scissors");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+
+        if (userInput.toLowerCase().equals("r") && computerValue == 1) {
+
+        }
+
+
     }
 }
